1733b3a9ad2a878b4df8e53ce2d8822baf5c387e
Emiel <i390838@fhict.nl>
2021-05-12 15:48:35 +0200

    dasd

--- baseServer.py
+++ baseServer.py
@@ -1,48 +1,47 @@
-#!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
+import random
 
 class S(BaseHTTPRequestHandler):
-    def _set_response(self):
+    def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self._set_headers()
+        f = open("index.html", "r")
+        self.wfile.write(f.read())
+
+    def do_HEAD(self):
+        self._set_headers()
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+        self._set_headers()
+        print( "in post method")
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
 
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        self.send_response(200)
+        self.end_headers()
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
+        data = simplejson.loads(self.data_string)
+        with open("test123456.json", "w") as outfile:
+            simplejson.dump(data, outfile)
+        print( "{}".format(data))
+        f = open("for_presen.py")
+        self.wfile.write(f.read())
+        return
+
+
+def run(server_class=HTTPServer, handler_class=S, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+    print ('Starting httpd...')
+    httpd.serve_forever()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+if len(argv) == 2:
+    run(port=int(argv[1]))
+else:
+    run()
