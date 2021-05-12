0384921cd0ae4f7d6f6e4625ff8556eef5b1dfb2
Emiel <i390838@fhict.nl>
2021-05-12 21:13:40 +0200

    add team change

--- gamepadtest.js
+++ gamepadtest.js
@@ -1,14 +1,8 @@
-/*
- * Gamepad API Test
- * Written in 2013 by Ted Mielczarek <ted@mielczarek.org>
- *
- * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
- */
+
 var haveEvents = 'GamepadEvent' in window;
 var haveWebkitEvents = 'WebKitGamepadEvent' in window;
 var controllers = {};
+var team = 0
 var rAF = window.mozRequestAnimationFrame ||
   window.webkitRequestAnimationFrame ||
   window.requestAnimationFrame;
@@ -18,7 +12,7 @@ function sendPost(theUrl, data){
   var url = "192.168.1.136:8000";
   var method = "POST";
   var postData = data;
-  //var postData = "Some data";
+  
   var shouldBeAsync = true;
   var request = new XMLHttpRequest();
   request.onload = function () {
@@ -29,6 +23,17 @@ function sendPost(theUrl, data){
   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   request.send(postData);
 }
+
+function changeTeam()
+{
+	if(team == 0)
+	{
+		team = 1;
+	}
+	else()
+	{
+		team = 0;
+	}
 
 function httpGet(theUrl)
 {
@@ -44,37 +49,7 @@ function connecthandler(e) {
 }
 function addgamepad(gamepad) {
   controllers[gamepad.index] = gamepad; var d = document.createElement("div");
-  /*
-  d.setAttribute("id", "controller" + gamepad.index);
-  var t = document.createElement("h1");
-  t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
-  d.appendChild(t);
-  var b = document.createElement("div");
-  b.className = "buttons";
-  for (var i=0; i<gamepad.buttons.length; i++) {
-    var e = document.createElement("span");
-    e.className = "button";
-    //e.id = "b" + i;
-    e.innerHTML = i;
-    b.appendChild(e);
-  }
-  d.appendChild(b);
-  var a = document.createElement("div");
-  a.className = "axes";
-  for (i=0; i<gamepad.axes.length; i++) {
-    e = document.createElement("meter");
-    e.className = "axis";
-    //e.id = "a" + i;
-    e.setAttribute("min", "-1");
-    e.setAttribute("max", "1");
-    e.setAttribute("value", "0");
-    e.innerHTML = i;
-    a.appendChild(e);
-  }
-  d.appendChild(a);
-  document.getElementById("start").style.display = "none";
-  document.body.appendChild(d);
-  */
+  
   rAF(updateStatus);
 }
 
@@ -94,47 +69,14 @@ function updateStatus() {
     var controller = controllers[j];
 	
 	let data = new Array(5);
-    data[0] = 1
+    data[0] = team
     data[1] = controller.axes[0].toFixed(4)
     data[2] = controller.axes[1].toFixed(4)
     data[3] = controller.axes[2].toFixed(4)
     data[4] = controller.axes[3].toFixed(4)
  
   sendPost("192.168.1.136:8000",data);
-	/*
-    var d = document.getElementById("controller" + j);
-    var buttons = d.getElementsByClassName("button");
-    for (var i=0; i<controller.buttons.length; i++) {
-      var b = buttons[i];
-      var val = controller.buttons[i];
-      var pressed = val == 1.0;
-      var touched = false;
-      if (typeof(val) == "object") {
-        pressed = val.pressed;
-        if ('touched' in val) {
-          touched = val.touched;
-        }
-        val = val.value;
-      }
-      var pct = Math.round(val * 100) + "%";
-	  
-      b.style.backgroundSize = pct + " " + pct;
-      b.className = "button";
-      if (pressed) {
-        b.className += " pressed";
-      }
-      if (touched) {
-        b.className += " touched";
-      }
-    }
 	
-    var axes = d.getElementsByClassName("axis");
-    for (var i=0; i<controller.axes.length; i++) {
-      var a = axes[i];
-      a.innerHTML = i + ": " + controller.axes[i].toFixed(4);
-      a.setAttribute("value", controller.axes[i]);
-    }
-	*/
 	
   }
   
