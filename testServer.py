c3cd02389eec5234ae480351705ea10312ab850d
Emiel <i390838@fhict.nl>
2021-05-19 20:45:20 +0200

    s

--- testServer.py
+++ testServer.py
@@ -40,7 +40,7 @@ def startVideo(width,height, framerate, mode, quality):
     #print('./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x {} -y {} -fps {} -quality {} -ex {}"'.format(width, height,framerate, mode, quality))
     #cmd = 'explorer "https://google.com"'
     #os.system(cmd)
-    cmd = ('cd streamer && ./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x {} -y {} -fps {} -quality {} -ex {}"'.format(width, height,framerate, mode, quality))
+    cmd = ('cd streamer && ./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -x {} -y {} -fps {} -ex {} -quality {}"'.format(width, height,framerate, mode, quality))
     #process = subprocess.Popen(cmd, shell=True)
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid) 
     print(cmd)
