aec1b2fb2b307b697467d13dc43fbb79134230ec
Emiel <i390838@fhict.nl>
2021-05-21 09:54:25 +0200

    used hardware

--- testServer.py
+++ testServer.py
@@ -25,17 +25,17 @@ def decodeMessage(message):
         print(word)
     if(arr[0] == '0'):
         #print(arr[1])
-        usedHardware.usedHardware.moveMotor(0,arr[1])
-        usedHardware.usedHardware.moveMotor(1,arr[2])
-        usedHardware.usedHardware.moveMotor(4,arr[3])
-        usedHardware.usedHardware.moveMotor(5,arr[4])
+        usedHardware.moveMotor(0,arr[1])
+        usedHardware.moveMotor(1,arr[2])
+        usedHardware.moveMotor(4,arr[3])
+        usedHardware.moveMotor(5,arr[4])
 
     if(arr[0] == '1'):
         #print(arr[1])
-        usedHardware.usedHardware.moveMotor(2,arr[1])
-        usedHardware.usedHardware.moveMotor(3,arr[2])
-        usedHardware.usedHardware.moveMotor(6,arr[3])
-        usedHardware.usedHardware.moveMotor(7,arr[4])
+        usedHardware.moveMotor(2,arr[1])
+        usedHardware.moveMotor(3,arr[2])
+        usedHardware.moveMotor(6,arr[3])
+        usedHardware.moveMotor(7,arr[4])
 
     if(arr[0] == 'v'):
         startVideo(arr[1],arr[2],arr[3],arr[4],arr[5])
