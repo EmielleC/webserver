ae818621d2fa754175527966818ebe935e1ec5f6
Emiel <i390838@fhict.nl>
2021-05-21 10:06:07 +0200

    ser

--- simpleServo.py
+++ simpleServo.py
@@ -4,16 +4,18 @@ import interface
 
 def _map(x, in_min, in_max, out_min, out_max):
     return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+    
+ser = serial.Serial(
+    port = '/dev/ttyACM0',
+    baudrate = 115200,
+    parity = serial.PARITY_NONE,
+    bytesize = serial.EIGHTBITS,
+    timeout = 1
+    )
 
 class simpleServo(interface.motorHardware) :
     
-    ser = serial.Serial(
-        port = '/dev/ttyACM0',
-        baudrate = 115200,
-        parity = serial.PARITY_NONE,
-        bytesize = serial.EIGHTBITS,
-        timeout = 1
-    )
+    
 
     def setup():
         print("setting up==========================================")
@@ -21,6 +23,6 @@ class simpleServo(interface.motorHardware) :
     
         value = _map((float(radValue)*-1), -1.00, 1.00, 0, 180)
         
-        self.ser.write(('p %d %d\n' % (servo, value)).encode())
+        ser.write(('p %d %d\n' % (servo, value)).encode())
 
         print("Moving motor")
