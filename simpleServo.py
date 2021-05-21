3100b6440df67b1df82263a37219e8494252cc53
Emiel <i390838@fhict.nl>
2021-05-21 09:58:48 +0200

    value

--- simpleServo.py
+++ simpleServo.py
@@ -19,14 +19,14 @@ class simpleServo(interface.motorHardware) :
         print("setting up==========================================")
     def moveMotor(servo, radValue ):
     
-        rot2 = _map((float(arr[1])*-1), -1.00, 1.00, 0, 180)
-        lin2 = _map((float(arr[2])*-1), -1.00, 1.00, 0, 180)
-        rot1 = _map((float(arr[3])*-1), -1.00, 1.00, 0, 180)
-        lin1 = _map((float(arr[4])*-1), -1.00, 1.00, 0, 180)
+        value = _map((float(radValue)*-1), -1.00, 1.00, 0, 180)
+        #lin2 = _map((float(arr[2])*-1), -1.00, 1.00, 0, 180)
+        #rot1 = _map((float(arr[3])*-1), -1.00, 1.00, 0, 180)
+        #lin1 = _map((float(arr[4])*-1), -1.00, 1.00, 0, 180)
         
-        ser.write(('p %d %d\n' % (servo, lin1)).encode())
-        ser.write(('p %d %d\n' % (servo, rot1)).encode())
-        ser.write(('p %d %d\n' % (servo, lin2)).encode())
-        ser.write(('p %d %d\n' % (servo, rot2)).encode())
+        ser.write(('p %d %d\n' % (servo, value)).encode())
+        #ser.write(('p %d %d\n' % (servo, rot1)).encode())
+        #ser.write(('p %d %d\n' % (servo, lin2)).encode())
+        #ser.write(('p %d %d\n' % (servo, rot2)).encode())
         
         print("Moving motor")
