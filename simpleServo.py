49ad777c248453aceb89488fc8c2c7a4ecffebdc
Emiel <i390838@fhict.nl>
2021-06-22 15:40:01 +0200

    20 degres

--- simpleServo.py
+++ simpleServo.py
@@ -24,7 +24,7 @@ class simpleServo(interface.motorHardware) :
     def moveMotor(self, servo, radValue ):
         
         if (servo % 2) == 0:
-            value = self.map((float(radValue)*-1), -1.0, 1.0, 70, 90)
+            value = self.map((float(radValue)*-1), -1.0, 1.0, 70, 110)
         else:
             value = self.map((float(radValue)*-1), (math.pi / -2.0000), (math.pi / 2.0000), 30, 150)
         print(value)
